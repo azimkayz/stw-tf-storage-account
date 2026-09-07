737505ee70fb6a3bc4d6174f7907e16a8fabb96f
Kayz <kayzcloud@hotmail.com>
2026-09-07 05:56:52 +0100

    Update to storage_account_id for azurerm provider v5 compatibility

--- storage_container.tf
+++ storage_container.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_container" "this" {
   name                   = var.container_name
-  storage_account_name   = azurerm_storage_account.this.name
+  storage_account_id     = azurerm_storage_account.this.id
   container_access_type  = var.container_access_type
 }
